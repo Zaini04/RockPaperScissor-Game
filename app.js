5625ca8a2333fbc13e4b9fc9decb16ffc3ce7d17
ZAIN IQBAL <124054465+Zaini04@users.noreply.github.com>
2024-07-08 18:48:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+
+let userScore = 0;
+let compScore = 0;
+
+
+const Options = document.querySelectorAll(".Option");
+const msg = document.querySelector("#msg");
+
+const userScorePara=document.querySelector("#user-score");
+const compScorePara=document.querySelector("#comp-score");
+
+
+
+const showWinner = (userWin) =>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You Won `;
+        msg.style.backgroundColor = "green";
+    }else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You Lose`;
+        msg.style.backgroundColor = "red";
+    }
+}
+const drawGame = () =>{
+    msg.innerText = `Game was draw! Play Again`;
+    msg.style.backgroundColor = "#081b31";
+}
+
+const genCompChoice = () =>{
+    const choices = ["rock" , "paper" , "scissor"];
+    const randIdx = Math.floor(Math.random()*3);
+    return choices[randIdx];
+};
+const playGame = (userChoice) =>{
+     console.log("user choice is",userChoice);
+    const compChoice = genCompChoice();
+     console.log("comp choice is",compChoice);
+
+     if(userChoice === compChoice){
+        drawGame();
+     }else {
+        let userWin=true;
+     if(userChoice==="rock" && compChoice==="paper"){
+        userWin=false;
+     }else if(userChoice==="rock" && compChoice==="scissor"){
+        userWin=true;
+     }else if(userChoice==="paper" && compChoice==="scissor"){
+        userWin=false;
+     }else if(userChoice==="paper" && compChoice==="rock"){
+        userWin=true;
+     }else if(userChoice==="scissor" && compChoice==="paper"){
+        userWin=true;
+     }else if(userChoice==="scissor" && compChoice==="rock"){
+        userWin=fale;
+     }
+     showWinner(userWin)
+    }
+
+
+};
+
+
+
+Options.forEach((Option)  => {
+    Option.addEventListener("click" , () =>{
+        const userChoice = Option.getAttribute("id");
+        // console.log(userChoice," was clicked");
+        playGame(userChoice);
+    });
+});
+$('#rock , #paper , #scissors').on('click' , function(){
+   console.log("hello Zain");
+   $('#msg').fadeOut(300).fadeIn(300);
+});
